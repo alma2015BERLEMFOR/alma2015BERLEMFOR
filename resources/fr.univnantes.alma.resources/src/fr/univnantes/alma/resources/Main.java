f4933e7f836d00ed3da2ae080a4ecc6416238ceb
Maxime BERGEON <webmastercr3a@gmail.com>
2015-11-06 08:34:53 +0100

    Added source folder to project 'resources'

---
+++ resources/fr.univnantes.alma.resources/src/fr/univnantes/alma/resources/Main.java
@@ -0,0 +1,10 @@
+package fr.univnantes.alma.resources;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a default file so that git accepts to commit the src folder");
+	}
+
+}
